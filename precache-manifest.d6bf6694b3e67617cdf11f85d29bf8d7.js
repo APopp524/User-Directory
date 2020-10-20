f83b96c99bc40e70651555ddc8b52800227c0915
Andrew Popp <apopp524@gmail.com>
2020-10-20 15:57:58 -0500

    Updates

---
+++ precache-manifest.d6bf6694b3e67617cdf11f85d29bf8d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8759bea54d1b7826582f97d3668162",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "165dab1d1103183491ad",
+    "url": "/User-Directory/static/css/main.01ad8dcc.chunk.css"
+  },
+  {
+    "revision": "743dc4b5debcd561319b",
+    "url": "/User-Directory/static/js/2.dad093b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/User-Directory/static/js/2.dad093b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "165dab1d1103183491ad",
+    "url": "/User-Directory/static/js/main.4690ef67.chunk.js"
+  },
+  {
+    "revision": "f8d550d65fe1b61ea334",
+    "url": "/User-Directory/static/js/runtime-main.b3b293a7.js"
+  }
+]);
